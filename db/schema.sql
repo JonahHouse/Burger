851dd72ae268cb945562f60739d0a0adb5bb4300
Jonah Householder <jonahhouseholder@outlook.com>
2020-05-29 10:33:18 -0700

    Directory set up

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT(6) AUTO_INCREMENT PRIMARY KEY,
+burger_name CHAR(255) NOT NULL,
+devoured BOOL NOT NULL DEFAULT 0
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ('Chicken Bacon Burger'), ('Happy Meal Cheeseburger'), ('Bacon Double Cheeseburger');
